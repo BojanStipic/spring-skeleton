31afbaebc4979a82ab06d52f4c6fbdba4ecfe96c
BojanStipic <bojanstipic@gmail.com>
2021-06-11 13:25:53 +0200

    Disable logout redirect and return 200 instead

--- src/main/java/bojanstipic/skeleton/security/WebSecurity.java
+++ src/main/java/bojanstipic/skeleton/security/WebSecurity.java
@@ -2,14 +2,13 @@ package bojanstipic.skeleton.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -19,22 +18,24 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsService userDetailsService;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        final var csrfTokenRepository = new CookieCsrfTokenRepository();
-        csrfTokenRepository.setCookiePath("/");
-        csrfTokenRepository.setCookieHttpOnly(false);
-
         http
-            .csrf().csrfTokenRepository(csrfTokenRepository).and()
-            .logout();
+            .csrf(csrf -> csrf
+                .csrfTokenRepository(cookieCsrfTokenRepository())
+            )
+            .logout(logout -> logout
+                .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler())
+            );
     }
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+    @Bean
+    CookieCsrfTokenRepository cookieCsrfTokenRepository() {
+        final var cookieCsrfTokenRepository = new CookieCsrfTokenRepository();
+        cookieCsrfTokenRepository.setCookiePath("/");
+        cookieCsrfTokenRepository.setCookieHttpOnly(false);
+
+        return cookieCsrfTokenRepository;
     }
 
     @Bean
